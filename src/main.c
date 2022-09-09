6ea3518609097fb8b86b63bd78dfd12b3c5d36e9
jdesimone03 <josuedesimone@fecea.edu.ar>
2022-09-09 10:28:51 -0300

    Agregado base de la maquina de estados de agua

--- src/main.c
+++ src/main.c
@@ -12,31 +12,42 @@
 #include "user.h"
 
 // Definiciones de Tipos
-typedef enum{E_REPOSO, E_DETECTA, E_ACTIVO} estadoMEF_t;
+
+typedef enum {
+    E_REPOSO, E_DETECTA, E_ACTIVO, E_VACIO, E_MEDIO, E_LLENO
+} estadoMEF_t;
 
 // Definiciones de variables
 estadoMEF_t estadoActual;
+estadoMEF_t estadoActual2;
 
 // Funciones Internas
 void colorMEFInit(void);
 void colorMEF(void);
 
+void aguaMEFInit(void);
+void aguaMEF(void);
+
 // Funcion Principal
-void main(void){
+
+void main(void) {
     appInit();
     colorMEFInit();
-    while(1){
+    aguaMEFInit();
+    while (1) {
         colorMEF();
+        aguaMEF();
     }
 }
 
 // Otras Funciones
-void colorMEFInit(void){
+
+void colorMEFInit(void) {
     estadoActual = E_REPOSO;
 }
 
-void colorMEF(void){
-    switch (estadoActual){
+void colorMEF(void) {
+    switch (estadoActual) {
         case E_REPOSO:
             // Espera a que pase el tiempo limite o por un boton (para demostrar)
             // y pasa al estado de DETECTA
@@ -53,5 +64,28 @@ void colorMEF(void){
         default:
             // Si cualquier cosa falla, reinicia al estado principal
             colorMEFInit();
+    }
+}
+
+void aguaMEFInit(void) {
+    estadoActual2 = E_VACIO;
+}
+
+void aguaMEF(void) {
+    switch (estadoActual2) {
+        case E_VACIO:
+            //esta para que detecte si le queda poca o nada de comida en el almacen
+            //espera a que se llene el almacen para que pase de estado 
+            //pasa al estado MEDIO
+            break;
+        case E_MEDIO:
+            //detecta el nivel de la comida cuando llega a la mitad del almacen
+            //pasa al esatdo lleno
+            break;
+        case E_LLENO:
+            //esta lleno no rompas las bolas 
+            break;
+        default:
+            aguaMEFInit();
     }
 }
